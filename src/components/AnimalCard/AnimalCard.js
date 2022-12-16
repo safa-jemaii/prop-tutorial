8324e6d08c710a1e73c3899acb6d41f66041ea4c
safa-jemaii <safajemaii48@gmail.com>
2022-12-16 20:40:53 +0100

    first commit

---
+++ src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './AnimalCard.css'
+import PropTypes from 'prop-types';
+
+export default function AnimalCard({
+    additional,
+    diet,
+    name,
+    scientificName,
+    showAdditional,
+    size
+  })  {
+    AnimalCard.propTypes = {
+        additional: PropTypes.shape({
+          link: PropTypes.string,
+          notes: PropTypes.string
+        }),
+        diet: PropTypes.arrayOf(PropTypes.string).isRequired,
+        name: PropTypes.string.isRequired,
+        scientificName: PropTypes.string.isRequired,
+        showAdditional: PropTypes.func.isRequired,
+        size: PropTypes.number.isRequired,
+      }
+
+      AnimalCard.defaultProps = {
+        additional: {
+          notes: 'No Additional Information'
+        }
+      }
+
+    
+    return (
+        <div className="animal-wrapper">
+          <h2>{name}</h2>
+          <h3>{scientificName}</h3>
+          <h4>{size}kg</h4>
+          <div>{diet.join(', ')}.</div>
+          <button onClick={() => showAdditional(additional)}>More Info</button>
+        </div>
+      );
+}
